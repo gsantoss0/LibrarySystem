16b9da57f8d0bcf4ea28b1e2d5a0b5ad7c71d6f0
Guilherme Santos <guilhermecostaabreu2804@gmail.com>
2026-07-27 13:41:20 -0300

    ajustes finais

--- LibrarySystem/LibrarySystem/Forms/BooksForm.Designer.cs
+++ LibrarySystem/LibrarySystem/Forms/BooksForm.Designer.cs
@@ -1,18 +1,9 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Guilherme
- * Date: 25/07/2026
- * Time: 10:44
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+
 namespace LibrarySystem.Forms
 {
 	partial class BooksForm
 	{
-		/// <summary>
-		/// Designer variable used to keep track of non-visual components.
-		/// </summary>
+
 		private System.ComponentModel.IContainer components = null;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
@@ -20,11 +11,7 @@ namespace LibrarySystem.Forms
 		private System.Windows.Forms.DataGridView dgvBooks;
 		private System.Windows.Forms.PictureBox picSearch;
 		private System.Windows.Forms.Button btnExit;
-		
-		/// <summary>
-		/// Disposes resources used by the form.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
 		protected override void Dispose(bool disposing)
 		{
 			if (disposing) {
@@ -34,12 +21,7 @@ namespace LibrarySystem.Forms
 			}
 			base.Dispose(disposing);
 		}
-		
-		/// <summary>
-		/// This method is required for Windows Forms designer support.
-		/// Do not change the method contents inside the source code editor. The Forms designer might
-		/// not be able to load this method if it was changed manually.
-		/// </summary>
+
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BooksForm));
